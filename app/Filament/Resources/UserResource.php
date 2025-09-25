1c6b4a751d1c6119ec0d5ac1fada405ef8d418b7
Brian Stanley <bmstanley@precisionecontrol.com>
2025-09-25 16:43:30 -0400

    Setup user list with sort that breaks export

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Exports\UserExporter;
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\ExportAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Query\Expression;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        $table
+            ->columns([
+                TextColumn::make('name')
+                    ->sortable(
+                        query: fn(Builder $query, string $direction): Builder => $query->orderBy(new Expression("(select name from users u2 where u2.id = users.id)"), $direction),
+                    )
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                ExportAction::make()->exporter(UserExporter::class),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+
+        \Log::info($table->getQuery()->toRawSql());
+
+        return $table;
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
